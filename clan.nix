9486fac2239a187effdb9c95c203dcd96d5a7b8b
Ante Petrovic <petrovicante6@gmail.com>
2025-10-30 11:56:34 +0100

    add user

--- clan.nix
+++ clan.nix
@@ -6,6 +6,11 @@
   inventory.machines = {
     # Define machines here.
     # jon = { };
+  
+    enginseer = {
+      tags = [];
+    };
+
   };
 
   # Docs: See https://docs.clan.lol/reference/clanServices
@@ -20,8 +25,29 @@
         # Insert the public key that you want to use for SSH access.
         # All keys will have ssh access to all machines ("tags.all" means 'all machines').
         # Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
-        "admin-machine-1" = "__YOUR_PUBLIC_KEY__";
+        "dominus" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuTvHKw/dHSm0NLjCQsk/9sPyNRerLB/wWuwitVpvdg";
       };
+    };
+
+    apetrovic-user =  {
+      module.name = "users";
+      roles.default.tags.all = { };
+      roles.default.settings = {
+        user = "apetrovic";
+        prompt = true;
+        share = true;
+        groups = [
+          "wheel" # Allow using 'sudo'
+          "networkmanager" # Allows to manage network connections.
+          "video" # Allows to access video devices.
+          "input" # Allows to access input devices.
+          "kvm"
+          "adb"
+          "docker"
+        ];
+      };
+
+      roles.default.extraModules = [ ./users/jon/home.nix ]; # 
     };
 
     # Docs: https://docs.clan.lol/reference/clanServices/zerotier/
@@ -51,8 +77,8 @@
   # machines/jon/configuration.nix will be automatically imported.
   # See: https://docs.clan.lol/guides/more-machines/#automatic-registration
   machines = {
-    # jon = { config, ... }: {
-    #   environment.systemPackages = [ pkgs.asciinema ];
-    # };
+     enginseer = { config, pkgs, ... }: {
+       environment.systemPackages = [ pkgs.btop ];
+     };
   };
 }
