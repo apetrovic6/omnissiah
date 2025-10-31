e8e9b6954faf9d2ee313a6e1c92e89fc375f67b6
Ante Petrovic <petrovicante6@gmail.com>
2025-10-31 11:35:45 +0100

    env vars

--- modules/core/hyprland.nix
+++ modules/core/hyprland.nix
@@ -22,8 +22,25 @@ options.services.imperium.hyprland = {
   };
 
   environment.variables = {
-    GDK_BACKEND = "wayland,x11,*"
+    GDK_BACKEND = "wayland,x11,*";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    SDL_VIDEODRIVER = "wayland";
+    CLUTTER_BACKEND = "wayland";
+
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+
+    QT_AUTO_SCREEN_SCALE_FACTOR = 1;
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NIXOS_OZONE_WL = 1;
   };
+
+  hardware.nvidia.powerManagement.enable = true;
 
   programs.hyprland = {
     enable = true;
