ec9b84b7c58ae7fd3005d7a22a1df16a25bc3189
Ante Petrovic <petrovicante6@gmail.com>
2025-11-01 12:34:45 +0100

    walker themes

---
+++ users/apetrovic/walker/themes/ugala/default.nix
@@ -0,0 +1,14 @@
+{...}:
+{
+programs.walker.temes.ugala = {
+    # Check out the default css theme as an example https://github.com/abenz1267/walker/blob/master/resources/themes/default/style.css
+    style = " /* css */ ";
+
+    # Check out the default layouts for examples https://github.com/abenz1267/walker/tree/master/resources/themes/default
+    # layouts = {
+    #   "layout" = " <!-- xml --> ";
+    #   "item_calc" = " <!-- xml --> ";
+    #   # other provider layouts
+    # };
+  };
+}
