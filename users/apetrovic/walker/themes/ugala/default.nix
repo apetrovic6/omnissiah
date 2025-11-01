5b6d38b50840d00ed8cb6ff1ccdad671c68525c2
Ante Petrovic <petrovicante6@gmail.com>
2025-11-01 12:40:55 +0100

    walker themes

--- users/apetrovic/walker/themes/ugala/default.nix
+++ users/apetrovic/walker/themes/ugala/default.nix
@@ -1,8 +1,13 @@
-{...}:
+{config, ...}:
 {
 programs.walker.temes.ugala = {
     # Check out the default css theme as an example https://github.com/abenz1267/walker/blob/master/resources/themes/default/style.css
-    style = " /* css */ ";
+    style = ''
+      .content.container {
+        background-color: #fff;
+      }
+
+      '';
 
     # Check out the default layouts for examples https://github.com/abenz1267/walker/tree/master/resources/themes/default
     # layouts = {
