b9b5a04fee1f6270ee742eab9c5597d4c5a0de64
Ante Petrovic <petrovicante6@gmail.com>
2025-11-02 12:38:33 +0100

    move firefox config

---
+++ users/apetrovic/firefox/default.nix
@@ -0,0 +1,29 @@
+{...}:
+{
+  programs.firefox.enable = true;
+  programs.librewolf = {
+  enable = true;
+
+    profiles.apetrovic = {
+      isDefault = true;
+      id = 0;
+
+    containersForce = true;
+    containers = {
+      google   = { id = 1; };
+      meta     = { id = 2; };
+      work     = { id = 3; };
+      banking  = { id = 4; };
+      shopping = { id = 5; };
+      private  = { id = 6; };
+    };
+
+    # If you set extensions.settings here or in sub-attrs,
+    # and Home Manager warns it "overrides previous settings",
+    # add:
+     extensions = {
+        force = true;
+     };
+   };
+  };
+}
