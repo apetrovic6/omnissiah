1f79f4ad46bf6b00696294a4f15cfa3461f53852
Ante Petrovic <petrovicante6@gmail.com>
2025-11-04 15:30:10 +0100

    update service modules

--- service-modules/workstation.nix
+++ service-modules/workstation.nix
@@ -1,31 +1,75 @@
 {
   _class = "clan.service";
   manifest.name = "workstation";
+  manifest.readme = "";
 
   roles.default.description = "Base packages and services";
 
-  roles.default.perInstance.nixosModule = { lib, pkgs, ... }: {
+  roles.default.perInstance.nixosModule = { self, config, lib, pkgs, ... }: {
     imports = [
+      #self.inputs.stylix.nixosModules.stylix
+
       ../modules/core/flatpaks.nix
       ../modules/core/stylix.nix
-      ../modules/hyprland/hyprland.nix
+      ../modules/core/hyprland.nix
+      ../modules/core/bluetooth.nix
     ];
 
-    services.imperium.hyprland.enable = true;
+    services.magos.hyprland = {
+      enable = true;
+      nvidia = {
+        enable = true;
+        modesetting = true;
+        powerManagement = true;
+      };
+    };
+
+    services.magos.bluetooth.enable = true;
+
+    services.magos.stylix = {
+      enable = true;
+      image = ../wallpapers/lofi/3.jpg;
+    };
+
+    boot.plymouth = {
+      enable = true;
+    };
+
+    services.greetd = {
+      enable = true;
+      settings = {
+      default_session = {
+        user = "apetrovic";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland"; # start Hyprland with a TUI login manager
+      };
+    };
+  };
+
+
+    programs.regreet = {
+      enable = true;
+
+      settings = {
+        background.path = config.stylix.image;
+        timezone = "Europe/Zagreb";
+      };
+
+      font = lib.mkForce {
+        package = config.stylix.fonts.monospace.package;
+        name = config.stylix.fonts.monospace.name;
+        size = config.stylix.fonts.sizes.desktop;
+      };
+    };
 
     environment.systemPackages = with pkgs; [
-      alacritty
-      kitty
       ghostty
-      wezterm
-
+      xfce.thunar
       zathura
       file-roller
       obsidian
 
       brave
       librewolf-bin
-      firefox
       
       vlc
 
