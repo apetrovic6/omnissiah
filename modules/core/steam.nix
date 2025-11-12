29011ae2e89b27b7a874f861e62cb255146bac76
Ante Petrovic <petrovicante6@gmail.com>
2025-11-12 20:49:13 +0100

    add steam module

---
+++ modules/core/steam.nix
@@ -0,0 +1,34 @@
+{self, ...}:
+{
+  flake.nixosModules.steam = {
+  config, lib, pkgs, ...
+  }:
+    let
+          inherit (lib) mkIf mkOption mkEnableOption types;
+          cfg = config.services.imperium.steam;
+    in {
+
+    options.services.imperium.steam = {
+      enable = mkEnableOption "Enable Steam";
+    }
+
+config = mkIf cfg.enable {
+  programs = {
+
+    gamescope= {
+      enable = true;
+      capSysNice = true;
+    };
+
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+environment.systemPackages = with pkgs; [ mangohud];
+
+  };
+  
+};
+    
+    }
+;}
