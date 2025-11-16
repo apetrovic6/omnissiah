96adb39e4a2a87170d279cf18a10c4d7f5560047
Ante Petrovic <petrovicante6@gmail.com>
2025-11-16 18:40:08 +0100

    test

--- modules/core/attic-token.nix
+++
@@ -1,8 +0,0 @@
-  {config, ...}:
- {
-  clan.core.vars.attic-pull-token = {
-    description = "Attic pull-only token for accessing the binary cache.";
-    secret = true;  # mark as secret so clan treats it as sensitive
-    # type = "string";  # if `type` is supported; usually the default is fine
-  };
-};
