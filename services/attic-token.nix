836dca9e0922d87586dec57f65f3e0e4ede07629
Ante Petrovic <petrovicante6@gmail.com>
2025-11-16 18:53:50 +0100

    remove

--- services/attic-token.nix
+++
@@ -1,8 +0,0 @@
-{ ... }: {
-  clan.core.vars.attic-pull-token = {
-    description = "Attic pull-only token for accessing the binary cache.";
-    secret = true; # clan treats this as sensitive
-    # type = "string"; # optional if you want to be explicit
-  };
-}
-
