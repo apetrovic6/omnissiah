217bcd1bbdd6f690be8717b9fa4c8c3bd21c6e85
Ante Petrovic <petrovicante6@gmail.com>
2025-11-17 13:18:04 +0100

    test

---
+++ modules/vars/common.nix
@@ -0,0 +1,24 @@
+{...}:
+{
+    # Optional: choose where secrets go (defaults to "sops")
+    clan.core.vars.settings.secretStore = "sops";
+
+    clan.core.vars.generators.attic-pull-token = {
+      share = true;
+
+      files.token = {
+        # secret = true is the default, so this is actually optional
+        secret = true;
+        # You can also set owner/group/mode if a service user needs it
+        owner = "apetrovic";
+        # group = "some-service-group";
+         mode = "0400";
+      };
+
+      files.attic-substituter = {
+        secret = true;
+        owner = "apetrovic";
+      };
+    };
+
+  }
