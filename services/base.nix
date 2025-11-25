936ff807a82e31cb2bdaebd9704c9a713a979614
Ante Petrovic <petrovicante6@gmail.com>
2025-11-25 15:47:22 +0100

    add locale info

--- services/base.nix
+++ services/base.nix
@@ -18,6 +18,7 @@
 
     environment.systemPackages = with pkgs; [
       attic-client
+      tree
       sbctl
       btop
       pciutils
@@ -30,6 +31,21 @@
       killall
     ];
 
+
+    time.timeZone = "Europe/Zagreb";
+    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "hr_HR.UTF-8";
+      LC_IDENTIFICATION = "hr_HR.UTF-8";
+      LC_MEASUREMENT = "hr_HR.UTF-8";
+      LC_MONETARY = "hr_HR.UTF-8";
+      LC_NAME = "hr_HR.UTF-8";
+      LC_NUMERIC = "hr_HR.UTF-8";
+      LC_PAPER = "hr_HR.UTF-8";
+      LC_TELEPHONE = "hr_HR.UTF-8";
+      LC_TIME = "hr_HR.UTF-8";
+    };
+    
     fonts = {
       packages = with pkgs.nerd-fonts; [
         fira-code
