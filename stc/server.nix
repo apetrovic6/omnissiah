46c34bbaa7cd952036f4f0f34021b912315b61a1
Ante Petrovic <petrovicante6@gmail.com>
2025-11-27 16:34:02 +0100

    server

--- stc/server.nix
+++ stc/server.nix
@@ -11,14 +11,40 @@
     pkgs,
     ...
   }: {
-      imports = [
-        self.nixosModules.noosphere.audiobookshelf
-      ];
+    imports = [
+      self.nixosModules.noosphere
+    ];
 
+    environment.systemPackages = [pkgs.cowsay];
 
-      services.imperium.audiobookshelf = {
-        enable = true;
-        port = 8008;
+    services.imperium.audiobookshelf = {
+      enable = true;
+      port = 8008;
+    };
+
+    networking.firewall.allowedTCPPorts = [80 443 2222 53];
+    networking.firewall.allowedUDPPorts = [80 443 53];
+    # Or disable the firewall altogether.
+    networking.firewall.enable = true;
+
+  users.groups.media = {};
+
+    
+
+    services.imperium.navidrome = {
+      enable = true;
+      port = 8888;
+      openFirewall = true;
+      user = "apetrovic";
+    };
+
+    services.imperium.caddy = {
+      enable = true;
+
+      package = pkgs.caddy.withPlugins {
+        plugins = ["github.com/caddy-dns/cloudflare@v0.2.1"];
+        hash = "sha256-Dvifm7rRwFfgXfcYvXcPDNlMaoxKd5h4mHEK6kJ+T4A=";
       };
     };
+  };
 }
