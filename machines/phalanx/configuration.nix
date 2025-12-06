55101e018823919caed697c85ccfe3c421788451
apetrovic <petrovicante6@gmail.com>
2025-12-06 18:14:51 +0100

    add config

---
+++ machines/phalanx/configuration.nix
@@ -0,0 +1,113 @@
+{
+  self,
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  diskId = "/dev/disk/by-id/nvme-WD_BLACK_SN770_2TB_23462Z801872";
+in {
+  imports = [
+    self.inputs.nix-flatpak.nixosModules.nix-flatpak
+    self.inputs.nixos-hardware.nixosModules.common-cpu-amd
+    self.inputs.nixos-hardware.nixosModules.common-cpu-pstate
+    self.inputs.nixos-hardware.nixosModules.common-gpu-nvidia
+    self.nixosModules.impermanence
+    self.nixosModules.zram
+    self.nixosModules.lanzaboote
+    self.nixosModules.vars
+    self.nixosModules.smb
+  ];
+
+  # nix = {
+  #   extraOptions = ''
+  #     !include ${config.clan.core.vars.generators.attic-pull-token.files.token.path}
+  #     netrc-file = /home/apetrovic/.config/nix/netrc
+  #   '';
+
+  #   settings = {
+  #     trusted-public-keys = [
+  #       "manjo:NYye+6m7jUVm3d9GUoIjXeX55/sz9xnRP/gl8THza6k="
+  #     ];
+  #   };
+  # };
+  #
+
+  services.imperium.smb.enable = true;
+
+  services.imperium.smb.hosts.manjaca = {
+    host = "192.168.1.61";
+    credentialsVarName = "manjaca-nas-credentials";
+
+    shares.games = {
+      shareName = "games";
+      mountPoint = "/mnt/nas/games";
+    };
+
+    shares."3DPrinting" = {
+      shareName = "3DPrinting";
+      mountPoint = "/mnt/nas/3dprinting";
+    };
+
+    shares.home = {
+      shareName = "home";
+      mountPoint = "/mnt/nas/home";
+    };
+
+    shares.data = {
+      mountPoint = "/mnt/nas/data";
+    };
+
+    shares.uevault = {
+      shareName = "UE_VAULT";
+      mountPoint = "/mnt/nas/ue_vault";
+    };
+
+    shares.docker = {
+      mountPoint = "/mnt/nas/ue_vault";
+    };
+  };
+
+  services.imperium.lanzaboote.enable = false;
+  services.imperium.zram.enable = true;
+
+  hardware.nvidia.powerManagement.enable = true;
+
+  services.imperium.impermanence = {
+    enable = true;
+  };
+
+  users.mutableUsers = false;
+  services.dbus.enable = true;
+  security.polkit.enable = true;
+
+  # systemd Stage 1: if enabled, it handles unlocking of LUKS-encrypted volumes during boot.
+  boot.initrd.luks.devices = {
+    cryptroot = {
+      device = "/dev/disk/by-partlabel/luks";
+      allowDiscards = true;
+    };
+  };
+
+  # This complements using zram, putting /tmp on RAM
+  boot = {
+    tmp = {
+      useTmpfs = true;
+      tmpfsSize = "50%";
+    };
+  };
+
+  # Enable autoScrub for btrfs
+  services.btrfs.autoScrub = {
+    enable = true;
+    interval = "weekly";
+    fileSystems = ["/"];
+  };
+
+  boot.loader = {
+    efi.canTouchEfiVariables = false;
+    grub.devices = [
+      diskId
+    ];
+  };
+}
---
+++ machines/phalanx/disko.nix
@@ -0,0 +1,102 @@
+# ---
+# schema = "single-disk"
+# [placeholders]
+# mainDisk = "/dev/disk/by-id/nvme-WD_PC_SN560_SDDPNQE-1T00-1102_23461C801092"
+# ---
+# This file was automatically generated!
+# CHANGING this configuration requires wiping and reinstalling the machine
+{lib, ...}: let
+  diskId = "/dev/disk/by-id/nvme-WD_BLACK_SN770_2TB_23462Z801872";
+in {
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.enable = false;
+
+  swapDevices = [
+    {
+      device = "/persist/swap/swapfile";
+      size = 18 * 1024; # Size in MB (18GB)
+      # or
+      # size = 16384; # Size in MB (16G);
+    }
+  ];
+
+  disko.devices = {
+    disk = {
+      nvme0n1 = {
+        type = "disk";
+        # Make sure this is correct with `lsblk`
+        device = diskId;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              label = "boot";
+              name = "ESP";
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            luks = {
+              size = "100%";
+              label = "luks";
+              content = {
+                type = "luks";
+                name = "cryptroot";
+                content = {
+                  type = "btrfs";
+                  extraArgs = ["-L" "nixos" "-f"];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = ["subvol=root" "compress=zstd" "noatime"];
+                    };
+                    "/root-blank" = {
+                      mountOptions = ["subvol=root-blank" "nodatacow" "noatime"];
+                    };
+                    # "/home" = {
+                    #   mountpoint = "/home";
+                    #   mountOptions = ["subvol=home" "compress=zstd" "noatime"];
+                    # };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
+                    };
+                    "/persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
+                    };
+                    "/log" = {
+                      mountpoint = "/var/log";
+                      mountOptions = ["subvol=log" "compress=zstd" "noatime"];
+                    };
+                    "/lib" = {
+                      mountpoint = "/var/lib";
+                      mountOptions = ["subvol=lib" "compress=zstd" "noatime"];
+                    };
+                    "/persist/swap" = {
+                      mountpoint = "/persist/swap";
+                      mountOptions = ["subvol=swap" "noatime" "nodatacow" "compress=no"];
+                      swap.swapfile.size = "64G";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
+  fileSystems."/persist".neededForBoot = true;
+  fileSystems."/var/log".neededForBoot = true;
+  fileSystems."/var/lib".neededForBoot = true; # };
+}
