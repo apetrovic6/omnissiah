45b437ce55ec5a534278bd451a8c75538911e1f4
apetrovic <petrovicante6@gmail.com>
2025-12-25 20:32:19 +0100

    update

--- taghmata/env/prod.nix
+++ taghmata/env/prod.nix
@@ -101,6 +101,22 @@
 
     helm.releases.sops-secrets-operator = {
       chart = charts.isindir.sops-secrets-operator;
+      values = {
+        secretsAsFiles = [
+          {
+            mountPath = "/etc/sops-age-key-file";
+            name = "sops-age-key-file";
+            secretName = "sops-age-key-file";
+          }
+        ];
+
+        extraEnv = [
+          {
+            name = "SOPS_AGE_KEY_FILE";
+            value = "/etc/sops-age-key-file/key";
+          }
+        ];
+      };
     };
   };
 
