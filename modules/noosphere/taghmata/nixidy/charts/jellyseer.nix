8847bf43ca2c1798190fe00503e7c3f368587a41
apetrovic <petrovicante6@gmail.com>
2025-12-27 13:17:01 +0100

    add custom chart

---
+++ modules/noosphere/taghmata/nixidy/charts/jellyseer.nix
@@ -0,0 +1,7 @@
+{lib,...}:
+{
+            repo = "oci://ghcr.io/fallenbagel/jellyseerr/jellyseerr-chart";
+            chart = "seerr-chart";
+            version = "3.0.0";
+            chartHash = lib.fakeHash;
+}
