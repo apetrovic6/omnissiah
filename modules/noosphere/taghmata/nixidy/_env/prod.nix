75e7d021912690809c0b14d8731a0911a98f038a
apetrovic <petrovicante6@gmail.com>
2025-12-27 20:20:52 +0100

    move longhorn to apps

--- modules/noosphere/taghmata/nixidy/_env/prod.nix
+++ modules/noosphere/taghmata/nixidy/_env/prod.nix
@@ -232,34 +232,7 @@
   #   };
   # };
 
-  applications.longhorn = let
-    namespace = "longhorn-system";
-  in {
-    inherit namespace;
-    createNamespace = true;
 
-    helm.releases.longhorn = {
-      chart = charts.longhorn.longhorn;
-      values = {
-        longhorn = {
-          preUpgradeChecker.jobEnabled = false;
-          persistance.defaultClassReplicaCount = 2;
-        };
-      };
-    };
-
-    resources.storageClasses.longhorn-cnpg-strict-local = {
-      provisioner = "driver.longhorn.io";
-      allowVolumeExpansion = true;
-      reclaimPolicy = "Delete";
-      parameters = {
-        numberOfReplicas = "1";
-        dataLocality = "strict-local";
-        staleReplicaTimeout = "2880"; # 48h
-        fsType = "ext4";
-      };
-    };
-  };
 
   applications.cert-manager = let
     namespace = "cert-manager";
