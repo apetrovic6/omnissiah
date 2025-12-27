4db62d0b3067566c2f1b41639d7f834ea14b6f56
apetrovic <petrovicante6@gmail.com>
2025-12-27 13:31:18 +0100

    test

--- modules/noosphere/taghmata/nixidy/charts/seerr/default.nix
+++ modules/noosphere/taghmata/nixidy/charts/seerr/default.nix
@@ -1,6 +1,6 @@
-{lib, ...}: {
-  repo = "oci://ghcr.io/fallenbagel/jellyseerr/jellyseerr-chart";
-  chart = "jellyseer-chart";
-  version = "3.0.0";
-  chartHash = lib.fakeHash;
+{
+  "repo" = "https://raw.githubusercontent.com/seerr-team/seerr/tree/develop/charts";
+  "chart" = "seerr-chart";
+  "version" = "3.0.0";
+  "chartHash" = "";
 }
