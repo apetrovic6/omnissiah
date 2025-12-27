89fe7c439d23a13cd9cd159a48b39dbb4e951a76
apetrovic <petrovicante6@gmail.com>
2025-12-27 21:03:07 +0100

    add hash

--- modules/noosphere/taghmata/nixidy/_env/apps/alloy/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/alloy/default.nix
@@ -4,14 +4,13 @@
     createNamespace = true;
     helm.releases.alloy = {
       chart = lib.helm.downloadHelmChart {
-
-  repo = "https://grafana.github.io/helm-charts";
-  chart = "alloy-operator";
-  version = "1.5.1";
-  chartHash = "";
-
+        repo = "https://grafana.github.io/helm-charts";
+        chart = "alloy-operator";
+        version = "0.3.15";
+        chartHash = "sha256-hallQsrgu6gnKE5jxkPEPWUh28R2E0VykcuTw0kXfq0=";
       };
       values = {
+        replicaCount = 3;
       };
     };
   };
