d4e19b57b437bfdd3fcaf4ea8d1d0fd8ae848cd3
apetrovic <petrovicante6@gmail.com>
2025-12-28 12:56:24 +0100

    add prometheus sta.ck

---
+++ modules/noosphere/taghmata/nixidy/_env/apps/prometheus-stack/default.nix
@@ -0,0 +1,20 @@
+{charts,...}: {
+
+  applications.prometheus = {
+    namespace = "observability";
+    createNamespace = true;
+
+    helm.releases.kube-prometheus-stack = {
+      chart = charts.prometheus-community.kube-prometheus-stack;
+
+      values = {
+        # ingress = {
+        #   enabled = true;
+        #   ingressClassName = "traefik";
+        #   hosts = 
+        # };
+      };
+    };
+  };
+
+}
