8a8813a47ab59b3f3c3d6e79f65c8fac76efdb98
apetrovic <petrovicante6@gmail.com>
2025-12-28 11:53:59 +0100

    remove charts dir

--- modules/noosphere/taghmata/nixidy/_env/charts/alloy-operator/default.nix
+++
@@ -1,6 +0,0 @@
-{
-        repo = "https://grafana.github.io/helm-charts";
-        chart = "alloy-operator";
-        version = "0.3.15";
-        chartHash = "sha256-hallQsrgu6gnKE5jxkPEPWUh28R2E0VykcuTw0kXfq0=";
-      }
