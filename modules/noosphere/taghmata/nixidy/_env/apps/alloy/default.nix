4c3bcc57b51ccd1971958f19ef1298f36bfcf70c
apetrovic <petrovicante6@gmail.com>
2025-12-28 13:09:36 +0100

    add ingress

--- modules/noosphere/taghmata/nixidy/_env/apps/alloy/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/alloy/default.nix
@@ -28,6 +28,44 @@
       ''
     ];
 
+    resources.ingresses.alloy-ip-root = {
+      metadata = {
+        namespace = "alloy";
+
+        annotations = {
+          "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure";
+        };
+      };
+
+      spec = {
+        ingressClassName = "traefik";
+
+        tls = [
+          {
+            secretName = "alloy-tls";
+            hosts = ["alloy.noosphere.uk"];
+          }
+        ];
+
+        rules = [
+          {
+            host = "alloy.noosphere.uk";
+            http.paths = [
+              {
+                path = "/";
+                pathType = "Prefix";
+                backend.service = {
+                  name = "default-alloy";
+                  port.number = 12345;
+                };
+              }
+            ];
+          }
+        ];
+      };
+    };
+
+
     resources.alloys.default = {
       metadata = {
         namespace = "observability";
@@ -64,27 +102,27 @@
           '';
         };
 
-        alloy = {
-          clustering = {
-            enabled = true;
-            name = "omnissiah";
-          };
-        };
-        controller = {
-          replicas = 3;
-          type = "daemonset";
-          # enableStatefulSetAutoDeletePVC = true;
-          # volumeClaimTemplates = {
-          #   metadata = ["alloy-wal"];
-          # };
-        };
+        # alloy = {
+        #   clustering = {
+        #     enabled = true;
+        #     name = "omnissiah";
+        #   };
+        # };
+        # controller = {
+        #   replicas = 3;
+        #   type = "daemonset";
+        #   # enableStatefulSetAutoDeletePVC = true;
+        #   # volumeClaimTemplates = {
+        #   #   metadata = ["alloy-wal"];
+        #   # };
+        # };
 
-        ingress = {
-          enabled = true;
-          ingressClassName = "traefik";
-          hosts = ["alloy.noosphere.uk"];
-          tls = ["alloy-tls"];
-        };
+        # ingress = {
+        #   enabled = true;
+        #   ingressClassName = "traefik";
+        #   hosts = ["alloy.noosphere.uk"];
+        #   tls = ["alloy-tls"];
+        # };
       };
     };
   };
