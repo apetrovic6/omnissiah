a8ae691963c39dec69f6c037b48c454dfc75a66e
apetrovic <petrovicante6@gmail.com>
2025-12-30 18:02:42 +0100

    add storage class

--- flake.nix
+++ flake.nix
@@ -289,6 +289,7 @@
             argocd
             nil
             nixd
+            garage_2
           ];
         };
       };
--- modules/noosphere/taghmata/nixidy/_env/apps/csi-driver-nfs/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/csi-driver-nfs/default.nix
@@ -8,27 +8,26 @@ in {
     helm.releases.csi-driver-nfs = {
       chart = charts.kubernetes-csi.csi-driver-nfs;
 
-      values = {
-        storageClasses = [
-          {
-            name = "nfs-csi";
-            annotations = {
-              "storageclass.kubernetes.io/is-default-class" = "true";
-            };
-
-            parameters = {
-              server = "192.168.1.61";
-              share = "/volume1/selhosted";
-              # subDir = "";
-            };
-            reclaimPolicy = "retain";
-
-            volumeBindingMode = "Immediate";
-            allowVolumeExpansion = true;
-            moundOptions = ["nfsvers=4.1"];
-          }
-        ];
-      };
     };
+resources.storageClasses.synology-nfs = {
+  provisioner = "nfs.csi.k8s.io";
+  reclaimPolicy = "Retain"; # or "Delete"
+  volumeBindingMode = "Immediate";
+  allowVolumeExpansion = true;
+
+  parameters = {
+    server = "192.168.1.61";
+    share  = "/volume1/selhosted";
+    subDir = "\${pvc.metadata.namespace}/\${pvc.metadata.name}/\${pv.metadata.name}";
+    # onDelete = "delete"; # delete|retain|archive
+    # mountPermissions = "0";
+  };
+
+  mountOptions = [
+    "hard"
+    "nfsvers=4.1"
+  ];
+};
+
   };
 }
