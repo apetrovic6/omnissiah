a2a90886350a52b5f4b086079567926f72ec39a9
apetrovic <petrovicante6@gmail.com>
2025-12-30 16:30:56 +0100

    actually generate manifests

--- modules/noosphere/taghmata/nixidy/_env/apps/csi-driver-nfs/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/csi-driver-nfs/default.nix
@@ -1,31 +1,34 @@
 {charts, ...}: let
   namespace = "csi-nfs";
 in {
-  helm.releases.csi-driver-nfs = {
-    inherit namespace;
-    createNamespace = true;
-    
-    chart = charts.kubernetes-csi.csi-driver-nfs;
+  applications.csi-driver-nfs = {
+      inherit namespace;
+      createNamespace = true;
 
-    values = {
-      storageClasses = [
-        {
-          name = "nfs-csi";
-          annotations = {
-            "storageclass.kubernetes.io/is-default-class" = "true";
-          };
+    helm.releases.csi-driver-nfs = {
 
-          parameters = {
-            server = "192.168.1.61";
-            share = "/volume1/selhosted";
-          };
-          reclaimPolicy = "retain";
+      chart = charts.kubernetes-csi.csi-driver-nfs;
 
-          volumeBindingMode = "Immediate";
-          allowVolumeExpansion = true;
-          moundOptions = ["nfsvers=4.1"];
-        }
-      ];
+      values = {
+        storageClasses = [
+          {
+            name = "nfs-csi";
+            annotations = {
+              "storageclass.kubernetes.io/is-default-class" = "true";
+            };
+
+            parameters = {
+              server = "192.168.1.61";
+              share = "/volume1/selhosted";
+            };
+            reclaimPolicy = "retain";
+
+            volumeBindingMode = "Immediate";
+            allowVolumeExpansion = true;
+            moundOptions = ["nfsvers=4.1"];
+          }
+        ];
+      };
     };
   };
 }
