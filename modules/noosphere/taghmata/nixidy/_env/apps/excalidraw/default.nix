6393276ace5a5df4dd9d6898a0f0c424c9e4b39e
apetrovic <petrovicante6@gmail.com>
2026-01-02 16:59:29 +0100

    excalidraw

--- modules/noosphere/taghmata/nixidy/_env/apps/excalidraw/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/excalidraw/default.nix
@@ -1,7 +1,6 @@
 {config, ...}: let
   namespace = "excalidraw";
   domain = config.noosphere.domain;
-
   labels = {app = "excalidraw";};
 in {
   applications.excalidraw = {
@@ -9,22 +8,29 @@ in {
     createNamespace = true;
 
     resources.deployments.excalidraw = {
-      metadata.labels = labels;
+      metadata = {
+        name = "excalidraw";
+        labels = labels;
+      };
 
       spec = {
         replicas = 3;
         selector.matchLabels = labels;
+
         template = {
           metadata.labels = labels;
+
           spec = {
             containers = [
               {
                 name = "excalidraw";
                 image = "excalidraw/excalidraw";
+
                 ports = [
                   {
-                    containerPort = 80;
                     name = "http";
+                    containerPort = 80;
+                    protocol = "TCP";
                   }
                 ];
               }
@@ -34,14 +40,73 @@ in {
       };
     };
 
-    resources.services.excalidraw = {
-      spec = {
-        selector = {
-          app = labels.app;
+    yamls = [
+      ''
+        apiVersion: cert-manager.io/v1
+        kind: Certificate
+        metadata:
+          name: excalidraw-tls
+          namespace: ${namespace}
+        spec:
+          secretName: excalidraw-tls
+          issuerRef:
+            kind: ClusterIssuer
+            name: letsencrypt-cloudflare
+          dnsNames:
+            - excalidraw.${domain}
+      ''
+    ];
+
+    resources.ingresses.excalidraw-ip-root = {
+      metadata = {
+        inherit namespace;
+
+        annotations = {
+          "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure";
         };
+      };
+
+      spec = {
+        ingressClassName = "traefik";
+
+        tls = [
+          {
+            secretName = "excalidraw-tls";
+            hosts = ["excalidraw.${domain}"];
+          }
+        ];
+
+        rules = [
+          {
+            host = "excalidraw.${domain}";
+            http.paths = [
+              {
+                path = "/";
+                pathType = "Prefix";
+                backend.service = {
+                  name = "excalidraw";
+                  port.number = 80;
+                };
+              }
+            ];
+          }
+        ];
+      };
+    };
+
+    resources.services.excalidraw = {
+      metadata = {
+        name = "excalidraw";
+        labels = labels;
+      };
+
+      spec = {
+        type = "ClusterIP";
+        selector = labels;
 
         ports = [
           {
+            name = "http";
             protocol = "TCP";
             port = 80;
             targetPort = "http";
