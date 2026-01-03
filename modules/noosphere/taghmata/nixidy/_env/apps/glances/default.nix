b529e65ff95979e255282c541f924f70360b7ef4
apetrovic <petrovicante6@gmail.com>
2026-01-03 20:11:32 +0100

    init glances

---
+++ modules/noosphere/taghmata/nixidy/_env/apps/glances/default.nix
@@ -0,0 +1,73 @@
+{config, ...}: let
+  namespace = "glances";
+  domain = config.noosphere.domain;
+  labels = {app = "glance";};
+  cfgDir = ./config;
+in {
+  applications.glance = {
+    inherit namespace;
+
+    resources.configMaps.glance-config = {
+      data = {
+        "glance.yml" = builtins.readFile (cfgDir + "/glance.yml");
+        "home.yml" = builtins.readFile (cfgDir + "/home.yml");
+      };
+    };
+
+    resources.deployments.glance = {
+      metadata.labels = labels;
+      spec = {
+        replicas = 1;
+        selector.matchLabels = labels;
+      };
+
+      template = {
+        medatadata.labels = labels;
+
+        spec = {
+          containers = [
+            {
+              name = "glance";
+              image = "glanceapp/glance:v0.8.4";
+              ports = [
+                {
+                  containerPort = 8080;
+                  name = "http";
+                }
+              ];
+
+              args = ["--config" "/app/config/glance.yml"];
+
+              volumeMounts = [
+                {
+                  name = "glance-config";
+                  mountPath = "/app/config";
+                }
+              ];
+              envFrom = [
+                {secretRef.name = "glance-secrets";} # created by your SOPS operator
+              ];
+            }
+          ];
+        };
+      };
+    };
+
+    resources.services.glance = {
+      # metadata = {};
+
+      spec = {
+        type = "ClusterIP";
+        selector = labels;
+
+        ports = [
+          {
+            name = "http";
+            protocol = "TCP";
+            port = "http";
+          }
+        ];
+      };
+    };
+  };
+}
