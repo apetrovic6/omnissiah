d7969356b0effa3a16fcb9f6d2457bb2b24f327f
apetrovic <petrovicante6@gmail.com>
2026-01-06 18:26:15 +0100

    barman cloud - init

--- modules/noosphere/taghmata/nixidy/_env/apps/cloudnative-pg/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/cloudnative-pg/default.nix
@@ -1,4 +1,21 @@
-{charts, ...}: {
+{
+  charts,
+  pkgs,
+  ...
+}: let
+  barmanVersion = "0.10.0";
+  barmanManifest = pkgs.fetchurl {
+    url = "https://github.com/cloudnative-pg/plugin-barman-cloud/releases/download/v${barmanVersion}/manifest.yaml";
+    hash = "sha256-9yjeqQqt490v60xLOTX6dLyHQwdjU8lwY1kbHQrUuKQ=";
+  };
+in {
+  applications.barman-cloud = {
+    namespace = "cnpg-system";
+    yamls = [
+      (builtins.readFile barmanManifest)
+    ];
+  };
+
   applications.cloudnativepg = let
     namespace = "cnpg-system";
   in {
