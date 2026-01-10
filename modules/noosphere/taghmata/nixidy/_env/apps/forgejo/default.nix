dd8dadbc52ef6a7b506237aabb7b87e2662040f7
apetrovic <petrovicante6@gmail.com>
2026-01-10 10:47:26 +0100

    forgejo - complete setup

--- modules/noosphere/taghmata/nixidy/_env/apps/forgejo/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/forgejo/default.nix
@@ -5,12 +5,14 @@
 }: let
   name = "forgejo";
   domain = config.noosphere.domain;
-  url = "${name}.${domain}";
+  url = "forge.${domain}";
   namespace = "${name}";
   db-cluster-name = "pg-${name}";
   objectStoreName = "${name}-object-store";
   barmanPluginName = "barman-cloud.cloudnative-pg.io";
   volumeSize = "2Gi";
+  forgejo-admin-secret = "forgejo-admin-secret";
+  forgejo-keycloak-oauth-secret = "forgejo-keycloak-oauth-secret";
 in {
   imports = [../../../_modules/templates/garage-object-store.nix];
 
@@ -19,8 +21,8 @@ in {
     createNamespace = true;
 
     yamls = [
-      (builtins.readFile ../../../../../../../vars/shared/woodpecker-default-agent-secret/woodpecker-default-agent-secret/value)
-      (builtins.readFile ../../../../../../../vars/shared/woodpecker-forgejo-oauth-secret/woodpecker-forgejo-oauth-secret/value)
+      (builtins.readFile ../../../../../../../vars/shared/${forgejo-admin-secret}/${forgejo-admin-secret}/value)
+      (builtins.readFile ../../../../../../../vars/shared/${forgejo-keycloak-oauth-secret}/${forgejo-keycloak-oauth-secret}/value)
     ];
 
     templates.garageObjectStore."${objectStoreName}" = {
@@ -28,8 +30,139 @@ in {
     };
 
     helm.releases.${name} = {
-      chart = charts.forgejo.forgejo;
-      values = {};
+      chart = charts.forgejo-helm.forgejo;
+      values = {
+        replicaCount = 3;
+        containerSecurityContext = {
+          allowPrivilegeEscalation = false;
+          capabilities = {
+            drop = ["all"];
+            #   # Add the SYS_CHROOT capability for root and rootless images if you intend to
+            #   # run pods on nodes that use the container runtime cri-o. Otherwise, you will
+            #   # get an error message from the SSH server that it is not possible to read from
+            #   # the repository.
+            #   # https://gitea.com/gitea/helm-chart/issues/161
+            add = ["SYS_CHROOT"];
+          };
+          privileged = false;
+          readOnlyRootFilesystem = true;
+          runAsGroup = 1000;
+          runAsNonRoot = true;
+          runAsUser = 1000;
+        };
+
+        ingress = {
+          enabled = true;
+          className = "traefik";
+          annotations = {
+            "cert-manager.io/cluster-issuer" = "letsencrypt-cloudflare";
+            "glance/name" = "Forgejo";
+            "glance/icon" = "di:forgejo";
+            "glance/url" = "https://${url}";
+            "glance/description" = "Git Forge";
+            "glance/id" = name;
+            "glance/parent" = name;
+            "category" = "gitops";
+          };
+          hosts = [
+            {
+              host = url;
+              paths = [
+                {
+                  path = "/";
+                  pathType = "Prefix";
+                  port = "http";
+                }
+              ];
+            }
+          ];
+
+          tls = [
+            {
+              secretName = "forgejo-tls";
+              hosts = [url];
+            }
+          ];
+        };
+
+        persistence = {
+          enabled = true;
+          storageClass = "longhorn";
+          accessModes = ["ReadWriteMany"];
+        };
+
+        service.ssh.annotations = {
+          "metallb.io/allow-shared-ip" = "192.168.1.240";
+        };
+
+        gitea = {
+          additionalConfigSources = [
+            # {secret.secretName = "${db-cluster-name}-app";}
+          ];
+
+          additionalConfigFromEnvs = [
+            {
+              name = "FORGEJO__database__DB_TYPE";
+              value = "postgres";
+            }
+
+            {
+              name = "FORGEJO__database__HOST";
+              value = "${db-cluster-name}-rw:5432";
+            }
+
+            {
+              name = "FORGEJO__database__NAME";
+              valueFrom.secretKeyRef = {
+                name = "${db-cluster-name}-app";
+                key = "dbname";
+              };
+            }
+
+            {
+              name = "FORGEJO__database__USER";
+              valueFrom.secretKeyRef = {
+                name = "${db-cluster-name}-app";
+                key = "username";
+              };
+            }
+
+            {
+              name = "FORGEJO__database__PASSWD";
+              valueFrom.secretKeyRef = {
+                name = "${db-cluster-name}-app";
+                key = "password";
+              };
+            }
+          ];
+
+          config = {
+            APP_NAME = "Imperial Forge";
+            database = {
+              DB_TYPE = "postgres";
+              HOST = "${db-cluster-name}-rw";
+            };
+
+            openid = {};
+          };
+
+          admin.existingSecret = forgejo-admin-secret;
+
+          metrics = {
+            enabled = true;
+            serviceMonitor.enabled = true;
+          };
+
+          oauth = [
+            {
+              name = "Keycloak";
+              existingSecret = forgejo-keycloak-oauth-secret;
+              autoDiscoverUrl = config.noosphere.sso.wellKnownUrl;
+              provider = "openidConnect";
+            }
+          ];
+        };
+      };
     };
 
     resources.scheduledBackups."${db-cluster-name}-scheduled-backup" = {
