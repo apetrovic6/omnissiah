53570d6f2b634034163eb950321093f25c4387d2
apetrovic <petrovicante6@gmail.com>
2026-02-03 13:19:31 +0100

    Set disk schema of machine: luna to single-disk

--- machines/luna/disko.nix
+++ machines/luna/disko.nix
@@ -1,21 +1,20 @@
 # ---
 # schema = "single-disk"
 # [placeholders]
-# mainDisk = "/dev/disk/by-id/nvme-CT500P310SSD8_25295198F3BE"
+# mainDisk = "/dev/disk/by-id/nvme-KINGSTON_SFYRD2000G_50026B7383CCF56C" 
 # ---
 # This file was automatically generated!
 # CHANGING this configuration requires wiping and reinstalling the machine
-let
-  diskId = "/dev/disk/by-id/nvme-CT500P310SSD8_25295198F3BE";
-in {
+{
+
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.enable = true;
   disko.devices = {
     disk = {
       main = {
-        name = "main-9da58821bc9b43ddb4b9a5964c83f148";
-        device = diskId;
+        name = "main-d793965c3c5a47b7965d963fafcb8203";
+        device = "/dev/disk/by-id/nvme-KINGSTON_SFYRD2000G_50026B7383CCF56C";
         type = "disk";
         content = {
           type = "gpt";
@@ -32,7 +31,7 @@ in {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = ["umask=0077"];
+                mountOptions = [ "umask=0077" ];
               };
             };
             root = {
