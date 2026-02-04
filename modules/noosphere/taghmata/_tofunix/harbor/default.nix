3b5717053cb0f87bd93819c304bcfdf3ce487332
apetrovic <petrovicante6@gmail.com>
2026-02-04 19:50:29 +0100

    tofu - move harbor to it's own file

---
+++ modules/noosphere/taghmata/_tofunix/harbor/default.nix
@@ -0,0 +1,47 @@
+{ref, ...}:
+let
+  secretsFile = toString ../../../../../vars/shared/tofunix-harbor-secrets/tofunix-harbor-secrets/value;
+in
+{
+
+  data.sops_file.secrets = {
+    source_file = secretsFile;
+    input_type = "yaml";
+  };
+
+  provider.harbor.default = {
+    url = "https://harbor.noosphere.uk";
+    username = "\${data.sops_file.secrets.data[\"harbor_username\"]}";
+    password = "\${data.sops_file.secrets.data[\"harbor_password\"]}";
+  };
+
+  resource.harbor_project.docker_cache = {
+    name = "docker_cache";
+    registry_id = ref.harbor_registry.docker.registry_id;
+    public = true;
+    vulnerability_scanning = true;
+    auto_sbom_generation = true;
+  };
+
+  resource.harbor_registry.docker = {
+    provider_name = "docker-hub";
+    name = "docker_cache";
+    endpoint_url = "https://hub.docker.com";
+  };
+
+
+  resource.harbor_project.ghcr_cache = {
+    name = "ghcr_cache";
+    registry_id = ref.harbor_registry.ghcr.registry_id;
+    public = true;
+    vulnerability_scanning = true;
+    auto_sbom_generation = true;
+  };
+
+  resource.harbor_registry.ghcr = {
+    provider_name = "github";
+    name = "github_cache";
+    endpoint_url = "https://ghcr.io";
+  };
+
+}
