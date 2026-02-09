ac0ad2626f7f9f0618a057a5507708e13f37f270
apetrovic <petrovicante6@gmail.com>
2026-02-09 20:47:10 +0100

    fix(longhorn): update the instance manager and manager images

--- modules/noosphere/taghmata/nixidy/_env/apps/longhorn/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/longhorn/default.nix
@@ -60,7 +60,14 @@ in {
     helm.releases.longhorn = {
       chart = charts.longhorn.longhorn;
       values = {
-        longhorn.preUpgradeChecker.jobEnabled = false;
+        longhorn.preUpgradeChecker = {
+          jobEnabled = false;
+          };
+          preUpgradeChecker.upgradeVersionCheck = false;        
+        image.longhorn = {
+          instanceManager.tag = "v1.11.0-hotfix-1";
+          manager.tag = "v1.11.0-hotfix-1";
+        };
         persistence = {
           defaultClassReplicaCount = 2;
           reclaimPolicy = "Retain";
