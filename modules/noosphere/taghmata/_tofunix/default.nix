f3290b688efd82e2268685389d81888307e48b04
apetrovic <petrovicante6@gmail.com>
2026-02-11 17:08:47 +0100

    feat(tofu): init s3 backend for tofu

--- modules/noosphere/taghmata/_tofunix/default.nix
+++ modules/noosphere/taghmata/_tofunix/default.nix
@@ -2,7 +2,24 @@
   imports = [
     ./harbor
     ./forgejo
+    ./projects
   ];
+
+  terraform.backend = {
+    s3 = {
+      bucket = "opentofu";
+      key = "terraform.tfstate";
+      region = "main";
+      endpoints = {
+        s3 = "https://s3.main.garage.noosphere.uk";
+      };
+      skip_credentials_validation = true;
+      skip_requesting_account_id = true;
+      skip_metadata_api_check = true;
+      skip_region_validation = true;
+      use_path_style = true;
+    };
+  };
 
   provider.sops.default = {};
 }
