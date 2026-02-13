a034414da3129c0a94f3cdcc2e8fe7cef5eebddb
apetrovic <petrovicante6@gmail.com>
2026-02-13 10:16:37 +0100

    refactor(garage-main): move more stuff in its own file

--- modules/noosphere/taghmata/nixidy/_env/apps/garage-operator/cluster-main/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/garage-operator/cluster-main/default.nix
@@ -1,7 +1,9 @@
-{domain, namespace, ...}:
-
 {
-
+  domain,
+  namespace,
+  ...
+}: {
+  applications.garage-operator = {
     resources.garageClusters.garage-main = {
       metadata = {inherit namespace;};
       spec = {
@@ -56,4 +58,164 @@
       };
     };
 
+    resources.garageKeys.opentofu = {
+      metadata = {inherit namespace;};
+      spec = {
+        clusterRef.name = "garage-main";
+      };
+    };
+
+    resources.garageBuckets.opentofu = {
+      metadata = {inherit namespace;};
+      spec = {
+        clusterRef.name = "garage-main";
+        keyPermissions = [
+          {
+            keyRef = "opentofu";
+            read = true;
+            write = true;
+            owner = true;
+          }
+        ];
+      };
+    };
+
+    resources.garageKeys.forgejo = {
+      metadata = {inherit namespace;};
+      spec = {
+        clusterRef.name = "garage-main";
+        secretTemplate = {
+          name = "forgejo-s3-secret-key";
+          namespace = "reflector";
+          accessKeyIdKey = "MINIO_ACCESS_KEY_ID";
+          secretAccessKeyKey = "MINIO_SECRET_ACCESS_KEY";
+          annotations = {
+            "reflector.v1.k8s.emberstack.com/reflection-allowed" = "true";
+            "reflector.v1.k8s.emberstack.com/reflection-allowed-namespaces" = "forgejo";
+            "reflector.v1.k8s.emberstack.com/reflection-auto-enabled" = "true";
+            "reflector.v1.k8s.emberstack.com/reflection-auto-namespaces" = "forgejo";
+          };
+        };
+      };
+    };
+
+    resources.garageBuckets.forgejo = {
+      metadata = {inherit namespace;};
+      spec = {
+        clusterRef.name = "garage-main";
+        keyPermissions = [
+          {
+            keyRef = "forgejo";
+            read = true;
+            write = true;
+            owner = true;
+          }
+        ];
+      };
+    };
+
+    resources.garageKeys.harbor = {
+      metadata = {inherit namespace;};
+      spec = {
+        clusterRef.name = "garage-main";
+      };
+    };
+
+    resources.garageBuckets.harbor = {
+      metadata = {inherit namespace;};
+      spec = {
+        clusterRef.name = "garage-main";
+        keyPermissions = [
+          {
+            keyRef = "harbor";
+            read = true;
+            write = true;
+            owner = true;
+          }
+        ];
+      };
+    };
+
+    resources.services.garage-s3-api = {
+      metadata = {inherit namespace;};
+      spec = {
+        type = "ClusterIP";
+        selector = {
+          "app.kubernetes.io/name" = "garage";
+          "app.kubernetes.io/instance" = "garage-main";
+        };
+        ports = [
+          {
+            name = "s3";
+            port = 3900;
+            targetPort = 3900;
+          }
+        ];
+      };
+    };
+
+    resources.services.garage-main-admin = {
+      metadata = {inherit namespace;};
+      spec = {
+        type = "ClusterIP";
+        selector = {
+          "app.kubernetes.io/name" = "garage";
+          "app.kubernetes.io/instance" = "garage-main";
+        };
+        ports = [
+          {
+            name = "admin";
+            port = 3903;
+            targetPort = 3903;
+          }
+        ];
+      };
+    };
+
+    resources.ingresses.garage-s3-api = {
+      metadata = {
+        inherit namespace;
+        annotations = {
+          "cert-manager.io/cluster-issuer" = "letsencrypt-cloudflare";
+        };
+      };
+      spec = {
+        ingressClassName = "traefik";
+        rules = [
+          {
+            host = "s3.main.garage.${domain}";
+            http.paths = [
+              {
+                path = "/";
+                pathType = "Prefix";
+                backend.service = {
+                  name = "garage-s3-api";
+                  port.number = 3900;
+                };
+              }
+            ];
+          }
+          {
+            host = "*.s3.main.garage.${domain}";
+            http.paths = [
+              {
+                path = "/";
+                pathType = "Prefix";
+                backend.service = {
+                  name = "garage-s3-api";
+                  port.number = 3900;
+                };
+              }
+            ];
+          }
+        ];
+        tls = [
+          {
+            secretName = "garage-operator-s3-tls";
+            hosts = ["s3.main.garage.${domain}" "*.s3.main.garage.${domain}"];
+          }
+        ];
+      };
+    };
+  };
 }
--- modules/noosphere/taghmata/nixidy/_env/apps/garage-operator/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/garage-operator/default.nix
@@ -6,9 +6,9 @@
   namespace = "garage-operator";
   domain = config.noosphere.domain;
 
-  cluster-main = import ./cluster-main { inherit config namespace domain; };
+  cluster-main = import ./cluster-main {inherit config namespace domain;};
 in {
-  imports = [ cluster-main ];
+  imports = [cluster-main];
   applications.garage-operator = {
     inherit namespace;
     createNamespace = true;
@@ -19,84 +19,6 @@ in {
       (builtins.readFile ../../../../../../../vars/shared/garage-backup-rpc-secret/garage-backup-rpc-secret/value)
       (builtins.readFile ../../../../../../../vars/shared/garage-backup-admin-token/garage-backup-admin-token/value)
     ];
-
-    resources.garageKeys.opentofu = {
-      metadata = {inherit namespace;};
-      spec = {
-        clusterRef.name = "garage-main";
-      };
-    };
-
-    resources.garageBuckets.opentofu = {
-      metadata = {inherit namespace;};
-      spec = {
-        clusterRef.name = "garage-main";
-        keyPermissions = [
-          {
-            keyRef = "opentofu";
-            read = true;
-            write = true;
-            owner = true;
-          }
-        ];
-      };
-    };
-
-    resources.garageKeys.forgejo = {
-      metadata = {inherit namespace;};
-      spec = {
-        clusterRef.name = "garage-main";
-        secretTemplate = {
-          name = "forgejo-s3-secret-key";
-          namespace = "reflector";
-          accessKeyIdKey = "MINIO_ACCESS_KEY_ID";
-          secretAccessKeyKey = "MINIO_SECRET_ACCESS_KEY";
-          annotations = {
-            "reflector.v1.k8s.emberstack.com/reflection-allowed" = "true";
-            "reflector.v1.k8s.emberstack.com/reflection-allowed-namespaces" = "forgejo";
-            "reflector.v1.k8s.emberstack.com/reflection-auto-enabled" = "true";
-            "reflector.v1.k8s.emberstack.com/reflection-auto-namespaces" = "forgejo";
-          };
-        };
-      };
-    };
-
-    resources.garageBuckets.forgejo = {
-      metadata = {inherit namespace;};
-      spec = {
-        clusterRef.name = "garage-main";
-        keyPermissions = [
-          {
-            keyRef = "forgejo";
-            read = true;
-            write = true;
-            owner = true;
-          }
-        ];
-      };
-    };
-
-    resources.garageKeys.harbor = {
-      metadata = {inherit namespace;};
-      spec = {
-        clusterRef.name = "garage-main";
-      };
-    };
-
-    resources.garageBuckets.harbor = {
-      metadata = {inherit namespace;};
-      spec = {
-        clusterRef.name = "garage-main";
-        keyPermissions = [
-          {
-            keyRef = "harbor";
-            read = true;
-            write = true;
-            owner = true;
-          }
-        ];
-      };
-    };
 
     resources.garageClusters.garage-backup = {
       metadata = {inherit namespace;};
@@ -115,8 +37,8 @@ in {
           };
         };
 
-        database = { engine = "sqlite"; };
-        replication = { factor = 3; };
+        database = {engine = "sqlite";};
+        replication = {factor = 3;};
 
         admin = {
           enabled = true;
@@ -152,90 +74,6 @@ in {
       };
     };
 
-
-    
-
-    resources.services.garage-s3-api = {
-      metadata = {inherit namespace;};
-      spec = {
-        type = "ClusterIP";
-        selector = {
-          "app.kubernetes.io/name" = "garage";
-          "app.kubernetes.io/instance" = "garage-main";
-        };
-        ports = [
-          {
-            name = "s3";
-            port = 3900;
-            targetPort = 3900;
-          }
-        ];
-      };
-    };
-
-    resources.services.garage-main-admin = {
-      metadata = {inherit namespace;};
-      spec = {
-        type = "ClusterIP";
-        selector = {
-          "app.kubernetes.io/name" = "garage";
-          "app.kubernetes.io/instance" = "garage-main";
-        };
-        ports = [
-          {
-            name = "admin";
-            port = 3903;
-            targetPort = 3903;
-          }
-        ];
-      };
-    };
-
-    resources.ingresses.garage-s3-api = {
-      metadata = {
-        inherit namespace;
-        annotations = {
-          "cert-manager.io/cluster-issuer" = "letsencrypt-cloudflare";
-        };
-      };
-      spec = {
-        ingressClassName = "traefik";
-        rules = [
-          {
-            host = "s3.main.garage.${domain}";
-            http.paths = [
-              {
-                path = "/";
-                pathType = "Prefix";
-                backend.service = {
-                  name = "garage-s3-api";
-                  port.number = 3900;
-                };
-              }
-            ];
-          }
-          {
-            host = "*.s3.main.garage.${domain}";
-            http.paths = [
-              {
-                path = "/";
-                pathType = "Prefix";
-                backend.service = {
-                  name = "garage-s3-api";
-                  port.number = 3900;
-                };
-              }
-            ];
-          }
-        ];
-        tls = [
-          {
-            secretName = "garage-operator-s3-tls";
-            hosts = ["s3.main.garage.${domain}" "*.s3.main.garage.${domain}"];
-          }
-        ];
-      };
-    };
 
     resources.services.garage-backup-s3-api = {
       metadata = {inherit namespace;};
