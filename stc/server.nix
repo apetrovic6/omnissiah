a780c1992654581c24a326fe2593a17cb5ed01ab
apetrovic <petrovicante6@gmail.com>
2026-05-06 07:46:38 +0200

    chore(caddy): update cloudflare plugin

--- stc/server.nix
+++ stc/server.nix
@@ -133,7 +133,7 @@
       openFirewall = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.3"];
-        hash = "sha256-20o+14cn/eeLuf1c8uGE1ODRZGC0oxocaIVlv4tFSvA=";
+        hash = "sha256-+htYZclHv9qI0TeHcBFvPkWzJVAZ5jqzTODrh4YmqXY=";
       };
     };
   };
