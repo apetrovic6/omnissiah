95315f0f47ea8678fec51ee1f94cf67e0f4a7c8f
apetrovic <petrovicante6@gmail.com>
2026-09-26 08:25:10 +0200

    feat(ncps): add extra caches

--- modules/noosphere/taghmata/nixidy/_env/apps/ncps/default.nix
+++ modules/noosphere/taghmata/nixidy/_env/apps/ncps/default.nix
@@ -54,6 +54,28 @@ in {
         mode = "statefulset";
 
         config = {
+          upstream.caches = [
+            {
+              url = "https://cache.nixos.org";
+              publicKey = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
+            }
+
+            {
+              url = "https://nix-community.cachix.org";
+              publicKey = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+            }
+
+            {
+              url = "https://nix-gaming.cachix.org";
+              publicKey = "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=";
+            }
+
+            {
+              url = "https://devenv.cachix.org";
+              publicKey = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+            }
+          ];
+
           hostname = "ncps.${domain}";
           server.addr = ":8501";
           signing = {
